afae290ebda59f728fe2a9a41824b707efe333ff
Dakar13 <ecalvamtz@gmail.com>
2020-06-05 18:15:22 -0500

    Estructura basica del servidor Apollo

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,31 +1,7 @@
-const cursos = [
-  {
-    titulo: "JavaScript Moderno Guía Definitiva Construye +10 Proyectos",
-    tecnologia: "JavaScript ES6",
-  },
-  {
-    titulo: "React La Guía Completa: Hooks Context Redux MERN +15 Apps",
-    tecnologia: "React",
-  },
-  {
-    titulo: "Node.js Bootcamp Desarrollo Web inc. MVC y Rest API´s",
-    tecnologia: "Node.js",
-  },
-  {
-    titulo: "ReactJS Avanzado FullStack React GraphQl y Apollo",
-    tecnologia: "React",
-  },
-];
-
 // Resolvers
 const resolvers = {
   Query: {
-    obtenerCursos: (_, { input }, ctx) => {
-      const resultado = cursos.filter(
-        (curso) => curso.tecnologia === input.tecnologia
-      );
-      return resultado;
-    },
+    obtenerCurso: () => "Algo",
   },
 };
 
